19cbc594044008b0f0980f91558f5f0158ccdc92
Maciej <maciej.gladki@gmail.com>
2018-09-25 16:46:42 +0200

    feat(): enable lombok

--- src/main/java/ch/cern/cms/daq/downtimeallocator/AllocationDTO.java
+++ src/main/java/ch/cern/cms/daq/downtimeallocator/AllocationDTO.java
@@ -1,10 +1,11 @@
 package ch.cern.cms.daq.downtimeallocator;
 
 import ch.cern.cms.daq.downtimeallocator.model.Downtime;
+import lombok.Data;
 
 import java.util.Map;
-import java.util.TreeMap;
 
+@Data
 public class AllocationDTO {
 
     private String title;
@@ -31,9 +32,7 @@ public class AllocationDTO {
 
     private Long shifterId;
 
-
     private Boolean shortFirstAction;
-
 
     private Boolean unidentified;
 
@@ -51,81 +50,6 @@ public class AllocationDTO {
 
     private String status;
 
-
-
-    public Boolean getShortFirstAction() {
-        return shortFirstAction;
-    }
-
-    public void setShortFirstAction(Boolean shortFirstAction) {
-        this.shortFirstAction = shortFirstAction;
-    }
-
-    public Boolean getUnidentified() {
-        return unidentified;
-    }
-
-    public void setUnidentified(Boolean unidentified) {
-        this.unidentified = unidentified;
-    }
-
-    public Boolean getStableBeams() {
-        return stableBeams;
-    }
-
-    public void setStableBeams(Boolean stableBeams) {
-        this.stableBeams = stableBeams;
-    }
-
-    public Boolean getAutomaticAvailable() {
-        return automaticAvailable;
-    }
-
-    public void setAutomaticAvailable(Boolean automaticAvailable) {
-        this.automaticAvailable = automaticAvailable;
-    }
-
-    public Boolean getAutomaticUsed() {
-        return automaticUsed;
-    }
-
-    public void setAutomaticUsed(Boolean automaticUsed) {
-        this.automaticUsed = automaticUsed;
-    }
-
-    public Boolean getRcmsRecovery() {
-        return rcmsRecovery;
-    }
-
-    public void setRcmsRecovery(Boolean rcmsRecovery) {
-        this.rcmsRecovery = rcmsRecovery;
-    }
-
-    public Boolean getSmoothDatatakingBefore() {
-        return smoothDatatakingBefore;
-    }
-
-    public void setSmoothDatatakingBefore(Boolean smoothDatatakingBefore) {
-        this.smoothDatatakingBefore = smoothDatatakingBefore;
-    }
-
-    public Boolean getIgnore() {
-        return ignore;
-    }
-
-    public void setIgnore(Boolean ignore) {
-        this.ignore = ignore;
-    }
-
-    public Map<String, Integer> getMergeMap() {
-        return mergeMap;
-    }
-
-    public void setMergeMap(Map<String, Integer> mergeMap) {
-
-        this.mergeMap = mergeMap;
-    }
-
     @Override
     public String toString() {
         return "AllocationDTO{" +
@@ -133,100 +57,4 @@ public class AllocationDTO {
                 '}';
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Map<String, Integer> getUnassigned() {
-        return unassigned;
-    }
-
-    public void setUnassigned(Map<String, Integer> unassigned) {
-        this.unassigned = unassigned;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public Downtime getDowntime() {
-        return downtime;
-    }
-
-    public void setDowntime(Downtime downtime) {
-        this.downtime = downtime;
-    }
-
-    public Map<String, Boolean> getAutomatic() {
-        return automatic;
-    }
-
-    public void setAutomatic(Map<String, Boolean> automatic) {
-        this.automatic = automatic;
-    }
-
-    public String getSubsystem() {
-        return subsystem;
-    }
-
-    public void setSubsystem(String subsystem) {
-        this.subsystem = subsystem;
-    }
-
-    public Long getShifterId() {
-        return shifterId;
-    }
-
-    public void setShifterId(Long shifterId) {
-        this.shifterId = shifterId;
-    }
-
-
-    public String getNext() {
-        return next;
-    }
-
-    public void setNext(String next) {
-        this.next = next;
-    }
-
-    public String getPrevious() {
-        return previous;
-    }
-
-    public void setPrevious(String previous) {
-        this.previous = previous;
-    }
-
-    public String getDeleteLink() {
-        return deleteLink;
-    }
-
-    public void setDeleteLink(String deleteLink) {
-        this.deleteLink = deleteLink;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
--- src/main/java/ch/cern/cms/daq/downtimeallocator/DowntimeDTO.java
+++ src/main/java/ch/cern/cms/daq/downtimeallocator/DowntimeDTO.java
@@ -1,66 +1,19 @@
 package ch.cern.cms.daq.downtimeallocator;
 
 import ch.cern.cms.daq.downtimeallocator.model.Downtime;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter @Setter
 public class DowntimeDTO {
-
 
     private Long all;
     private Long validated;
     private Long ignored;
     private Long neww;
     private Float percentage;
-
     private List<Downtime> downtimeList;
 
-    public Long getAll() {
-        return all;
-    }
-
-    public void setAll(Long all) {
-        this.all = all;
-    }
-
-    public Long getValidated() {
-        return validated;
-    }
-
-    public void setValidated(Long validated) {
-        this.validated = validated;
-    }
-
-    public Long getIgnored() {
-        return ignored;
-    }
-
-    public void setIgnored(Long ignored) {
-        this.ignored = ignored;
-    }
-
-    public Long getNeww() {
-        return neww;
-    }
-
-    public void setNeww(Long neww) {
-        this.neww = neww;
-    }
-
-    public Float getPercentage() {
-        return percentage;
-    }
-
-    public void setPercentage(Float percentage) {
-        this.percentage = percentage;
-    }
-
-
-    public List<Downtime> getDowntimeList() {
-        return downtimeList;
-    }
-
-    public void setDowntimeList(List<Downtime> downtimeList) {
-        this.downtimeList = downtimeList;
-    }
 }
